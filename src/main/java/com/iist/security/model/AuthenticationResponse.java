8b4c8d26277c07142fa53a62a20973ec1b364973
MohammedJasrin <mohammedjasrinmj@gmail.com>
2024-08-03 16:40:24 +0530

    JWTSecurity

---
+++ src/main/java/com/iist/security/model/AuthenticationResponse.java
@@ -0,0 +1,27 @@
+package com.iist.security.model;
+
+public class AuthenticationResponse {
+
+	private final String jwt;
+
+
+
+	public AuthenticationResponse(String jwt) {
+
+		
+
+		this.jwt = jwt;
+
+	}
+
+
+
+	public String getJwt() {
+
+		return jwt;
+
+	}
+
+
+
+}
